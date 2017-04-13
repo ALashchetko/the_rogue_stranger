c87697f0ac64d2f87d1ec3c91404b80f81d02d2a
ALashchetko <flatron_vi@mail.ru>
2017-04-13 23:55:19 +0300

    LowDevelopers-game-team/fix : fix flag

--- assets/js/flag.js
+++ assets/js/flag.js
@@ -19,7 +19,8 @@ Flag.prototype.constructor = Flag;
 Flag.prototype.update = function() {
     game.physics.arcade.collide(this, layer);
     this.animations.play('flag_idle');
-    if (player.y === this.y && player.x + 20 > this.x && player.x - 20 < this.x) endOfLevel();
+    if (player.y + 20 > this.y && player.y - 20 < this.y && player.x + 20 > this.x && player.x - 20 < this.x)
+    endOfLevel();
 };
 
 function endOfLevel() {
@@ -27,35 +28,43 @@ function endOfLevel() {
     rectangle = game.add.sprite(320, 240, 'rectangle');
     rectangle.anchor.setTo(0.5, 0.5);
     coinLeftSound = game.add.audio('coin_left_sound');
+    rectangle.fixedToCamera = true;
 
     heart_text_tmp = game.add.text(240, 197, "+1\t\t\t\t50", style_tmp);
     heart_text_tmp.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
     heart_text_tmp.anchor.setTo(0.5, 0.5);
     heart_text_tmp.visible = true;
+    heart_text_tmp.fixedToCamera = true;
 
     heart_tmp = game.add.sprite(230, 195, 'heart');
     heart_tmp.anchor.setTo(0.5, 0.5);
     heart_tmp.scale.setTo(0.2, 0.2);
+    heart_tmp.fixedToCamera = true;
 
     dagger_text_tmp = game.add.text(272, 237, "25", style_tmp);
     dagger_text_tmp.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
     dagger_text_tmp.visible = true;
+    dagger_text_tmp.fixedToCamera = true;
 
     dagger_tmp = game.add.sprite(230, 245, 'dagger_on_ground');
     dagger_tmp.anchor.setTo(0.5, 0.5);
     dagger_tmp.scale.setTo(1.5, 1.5);
+    dagger_tmp.fixedToCamera = true;
 
     coin_tmp1 = game.add.sprite(320, 195, 'coin_counter');
     coin_tmp1.scale.setTo(0.1, 0.1);
     coin_tmp1.anchor.setTo(0.5, 0.5);
+    coin_tmp1.fixedToCamera = true;
 
     coin_tmp2 = game.add.sprite(320, 245, 'coin_counter');
     coin_tmp2.scale.setTo(0.1, 0.1);
     coin_tmp2.anchor.setTo(0.5, 0.5);
+    coin_tmp2.fixedToCamera = true;
 
     next_level_text_tmp = game.add.text(200, 285, "Next level", style_tmp);
     next_level_text_tmp.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
     next_level_text_tmp.visible = true;
+    next_level_text_tmp.fixedToCamera = true;
 
     addLife_button = game.add.button(400, 190, 'check_mark', () => {
         if (coinsCount >= 50 && countOflifes < 5) {
@@ -68,6 +77,7 @@ function endOfLevel() {
     }, this);
     addLife_button.anchor.setTo(0.5, 0.5);
     addLife_button.scale.setTo(0.2, 0.2);
+    addLife_button.fixedToCamera = true;
 
     addWeapon_button = game.add.button(400, 240, 'check_mark', () => {
         if (coinsCount >= 25 && add_weapon != true) {
@@ -81,6 +91,7 @@ function endOfLevel() {
     }, this);
     addWeapon_button.anchor.setTo(0.5, 0.5);
     addWeapon_button.scale.setTo(0.2, 0.2);
+    addWeapon_button.fixedToCamera = true;
 
     nextLevel_button = game.add.button(400, 290, 'check_mark', () => {
         next_level = true;
@@ -88,6 +99,7 @@ function endOfLevel() {
     }, this);
     nextLevel_button.anchor.setTo(0.5, 0.5);
     nextLevel_button.scale.setTo(0.2, 0.2);
+    nextLevel_button.fixedToCamera = true;
 
     game.input.onDown.add(unpause, self);
 
